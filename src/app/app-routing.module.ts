29e76784fdf9b5406ad243557ec30ab110c50650
spelka <sebastian.pelka@hotmail.com>
2019-01-14 21:19:42 -0800

    Angular implementation of To Do list initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ReactiveFormsModule, FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgForm
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  todoArray=[];
+
+  addTodo(value)
+  {
+    this.todoArray.push(value);
+    console.log("Add Item: " + this.todoArray);
+  }
+
+  deleteTodo(todo)
+  {
+    console.log("Delete Item: " + todo); 
+    for (let i=0; i<= this.todoArray.length; i++)
+    {
+      if (todo = this.todoArray[i])
+      {
+        this.todoArray.splice(i,1);
+      }
+    }
+  }
+
+  // enable submitting the form with the enter key
+  todoSubmit(value:any)
+  {
+    if(value!=="")
+    {
+      this.todoArray.push(value.todo)
+      //this.todoForm.reset()
+    }
+    else
+    {
+      alert('Field required **')
+    }
+  
+  }
+
 }
+
