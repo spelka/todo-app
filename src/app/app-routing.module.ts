6cc2f729783ea9e99abb59fcc4ad4cbde220fc79
spelka <sebastian.pelka@hotmail.com>
2019-01-14 21:42:05 -0800

    moved dependencies to app.module. Reverted app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ReactiveFormsModule, FormsModule, NgForm } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes),
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    NgForm
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
